ca915e023b5805089079c5ce33a0afbbc5fd9518
Hitesh sharma <sharmahitesh9828@gmail.com>
2024-02-01 21:55:37 +0530

    Feat: setup abasic server using express and nodemon

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require('express');
+require("dotenv").config()
+const app=express();
+const port =process.env.PORT
+
+
+
+app.use(express.json());
+
+
+
+app.get('/',(req,res)=>{
+    try {
+        res.status(200).json({msg:"welcome you are on Home screen"})
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+})
+
+app.listen(port,async (req,res)=>{
+    try {
+        console.log(`your server is running on port ${port}`);
+    } catch (error) {
+        console.log(error.message)
+    }
+})
